0f3e1aa8579cab996311bc3b448cce9e6e06fe3b
Paolo <paolo.cataldo0722@gmail.com>
2026-03-28 19:56:33 +0100

    Login principale

---
+++ includes/check_login.php
@@ -0,0 +1,30 @@
+<?php
+
+include "../config.php";
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $password = md5($_POST['password']); 
+
+    $sql = "SELECT id, username, email, password, ruolo";
+    $sql .= " FROM utenti";
+    $sql .= " WHERE username = '$username' AND password = '$password'";
+
+    $query = mysqli_query($conn, $sql);
+
+    if (!$query) {
+        die("Errore query: " . mysqli_error($conn));
+    }
+
+    $result = mysqli_fetch_assoc($query);
+
+    if($result){
+        echo "Login effettuato con successo!";
+    } else{
+        echo "Credenziali errate!";
+        echo '<br><a href="../pages/login.php">Riprova login</a>';
+    }
+} else {
+    echo "Compila tutti i campi!";
+    echo '<br><a href="../pages/login.php">Torna al login</a>';
+}
