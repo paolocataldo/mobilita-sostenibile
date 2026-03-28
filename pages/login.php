d0738daf10555a072cd031b10ec839a23d7fc723
Paolo <paolo.cataldo0722@gmail.com>
2026-03-28 19:32:05 +0100

    Modifica css del Login

--- pages/login.php
+++ pages/login.php
@@ -1,24 +1,28 @@
 <!DOCTYPE html>
-
-
+<html>
 
 <head>
     <meta charset="UTF-8">
     <title>Form con sfondo</title>
-    <link rel="stylesheet" href="style.css">
-
+    <link rel="stylesheet" href="../css/login.css">
+    <script src="../js/login_script.js"></script>
 </head>
 
 <body>
-    <form class="mio-form">
+    <div class="mio-form">
 
-<label for="nome">nome:</label><br>
-<input type="text" name="nome" required><br><br>
+        <form action="../includes/check_login.php" method="POST">
 
-<label for="cognome">cognome:</label><br>
-<input type="text" name="cognome" required><br><br>
+            <label for="username">Username</label><br>
+            <input type="text" name="username" required><br><br>
 
-    </form>
+            <label for="password">Password</label><br>
+            <input type="password" name="password" required><br><br>
+
+            <input type="submit" value="Login">
+
+        </form>
+    </div>
 </body>
 
 </html>
