6c7f329c4de97641cba9ed0d45c3af9cd2149995
Paolo <paolo.cataldo0722@gmail.com>
2026-03-28 17:59:54 +0100

    primo commit

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <title>Form con sfondo</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <form class="mio-form">
+
+<label for="nome">nome:</label><br>
+<input type="text" name="nome" required><br><br>
+
+<label for="cognome">cognome:</label><br>
+<input type="text" name="cognome" required><br><br>
+
+    </form>
+</body>
+
+</html>
