c37a7c8880143a64ba694713046ad22d3575f2c4
Paolo <paolo.cataldo0722@gmail.com>
2026-03-28 19:58:35 +0100

    aggiunta commento sulle funzioni da implementare

--- includes/check_login.php
+++ includes/check_login.php
@@ -1,7 +1,7 @@
 <?php
 
 include "../config.php";
-
+//fare le funzioni principali: sessione, logout, registrazione
 if (isset($_POST['username']) && isset($_POST['password'])) {
     $username = $_POST['username'];
     $password = md5($_POST['password']); 
