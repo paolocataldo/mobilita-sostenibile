a2c4ed09fe7e448f8870c9fcf8ce4a242382a83f
Paolo <paolo.cataldo0722@gmail.com>
2026-03-29 13:01:04 +0200

    registrazione.php

--- pages/registrazione.php
+++ pages/registrazione.php
@@ -11,7 +11,17 @@
 <body>
     <div class="registrazione_form">
         <form action="registrazione_utente.php" method="POST">
-            
+            <label for="username">Username</label>
+            <input type="text" name="username" required><br><br>
+            <label for="email">Email</label>
+            <input type="email" name="email" pattern="[a-z0-9._%+\-]+@studenti\.itisavogadro\.it$" required><br><br> <!-- Sono valide solo le email istituzionali -->
+            <label for="conferma_email">Conferma Email</label>
+            <input type="email" name="email" pattern="[a-z0-9._%+\-]+@studenti\.itisavogadro\.it$" required><br><br>
+            <label for="password">Password</label>
+            <input type="password" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required><br><br>
+            <label for="conferma_password">Conferma Password</label>
+            <input type="password" name="conferma_password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"><br><br>
+
         </form>
     </div>
 </body>
