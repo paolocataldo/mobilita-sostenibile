2ed4b7f63a24616ad4390cb9e39683ab3580b910
Paolo <paolo.cataldo0722@gmail.com>
2026-04-09 16:57:29 +0200

    attivazione account

--- includes/check_login.php
+++ includes/check_login.php
@@ -7,7 +7,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
     $sql = "SELECT id, username, email, password, ruolo";
     $sql .= " FROM utenti";
-    $sql .= " WHERE username = '$username' AND password = '$password'";
+    $sql .= " WHERE username = '$username' AND password = '$password' AND account_activation_hash IS NULL";
 
     $query = mysqli_query($conn, $sql);
 
@@ -21,7 +21,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         header("Location: ../pages/pagina_privata.php");
         exit(); // Ferma lo script dopo il redirect
     } else{
-        echo "Credenziali errate!";
+        echo "Credenziali errate, o account non attivato";
         echo '<br><a href="../pages/login.php">Riprova login</a>';
     }
 } else {
